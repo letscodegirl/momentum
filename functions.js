fd1d203064466c953a6c0d1f8a278f011620dcfa
chloe <chloe.laureate@gmail.com>
2023-12-06 21:50:30 +0900

    Initial commit

---
+++ functions.js
@@ -0,0 +1,28 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+
+  sub: function (a, b) {
+    console.log(a - b);
+  },
+
+  div: function (a, b) {
+    console.log(a / b);
+  },
+
+  multi: function (a, b) {
+    console.log(a * b);
+  },
+
+  power: function (a, b) {
+    console.log(a ** b);
+  }
+
+};
+
+calculator.add(5, 1);
+calculator.sub(5, 1);
+calculator.div(5, 1);
+calculator.multi(5, 1);
+calculator.power(5, 1);
