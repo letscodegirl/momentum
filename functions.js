de8a89bdee0556929586486680066de1e4433ee4
chloe <chloe.laureate@gmail.com>
2023-12-07 17:31:36 +0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,9 +1,30 @@
-const age = 96;
-function calculateKrAge(ageofForeigner) {
-  ageofForeigner + 2;
-  return "hello";
-}
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
 
-const krAge = calculateKrAge(age); // returning this line with the result of line 3
+  sub: function (a, b) {
+    return a - b;
+  },
 
-console.log(krAge);
+  div: function (a, b) {
+    return a / b;
+  },
+
+  multi: function (a, b) {
+    return a * b;
+  },
+
+  power: function (a, b) {
+    return a ** b;
+  }
+
+};
+
+const addResult = calculator.add(4, 3);
+const subResult = calculator.sub(addResult, 10);
+const divResult = calculator.div(10, subResult);
+const multiResult = calculator.multi(divResult, addResult);
+const powerResult = calculator.power(divResult, subResult);
+
+console.log(addResult);
