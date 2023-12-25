d588fba196ae93b0855064f70382064d298cc493
chloe <chloe.laureate@gmail.com>
2023-12-25 21:00:34 +0900

    hi

--- functions.js
+++ functions.js
@@ -1,3 +1,9 @@
-const title = document.querySelector(".hello div");
+const title =애쳐
 
 console.log(title);
+
+//title is an array
+
+//querySelector: element를 css방식으로 검색 가능
+
+//cannot set property 'innertext' of null
